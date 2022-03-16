85b7b4ac97d5cedddfc20a5e759d9f4ef28449f5
ilya0 <iosovets@gmail.com>
2022-03-16 12:07:43 -0700

    renamed the functions and figureed out how to get the cookie value

--- circle/question2.js
+++ circle/question2.js
@@ -2,31 +2,61 @@
 <html>
 <head>
 <script>
-const loads;
 
-//set cookie to param
-function setCookie(v) {
+const loads;   //var to save cookie values from the browser
+
+
+
+
+
+function writeCookie(v) { //function to set the cookie value
+  
   document.cookie = "cookieCounter = " + v;
   console.log(document.cookie);
-}
+  }
 
 
-//run this on the page load
-  function start(){
-  	//get value of cookie
-	loads = getCookieValue("cookieCounter");
-	//double checking the load value
-	console.log("number of loads on cookie = " +loads);
-    var newload = loads++;
-    setCookie(newload);
-}
+
+
+  function readCookieValue(){
+   //get the value of a cookie
+   let cookievalue = "cookieCounter=";
+   let decodedCookie = decodeURIComponent(document.cookie); // decode the cookie
+   let ca = decodedCookie.split(';'); // split it into
+   
+   
+   for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(cookievalue) == 0) {
+      return c.substring(cookievalue.length, c.length);
+    }
+  }
+  return "";
+  };
+  
+
+
+  //run this on page start
+  function start(){    
+  	
+	loads = readCookieValue("cookieCounter"); //save the value of the current cookie
+	console.log("number of loads on cookie = " +loads); //double checking the load value
+    var newload = loads++; // add 1 to loads
+    writeCookie(newload); // set new cookie value
+  }
+
+
 
 
 </script>
 </head>
 
 <body onload="start()"></body>
-<button onclick="setCookie(5)">Click here to reset cookie</button>
+<button onclick="writeCookie(0)">Click here to reset cookie</button>
 </html>
 
 
